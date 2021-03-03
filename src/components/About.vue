62d6e2d6777a5a56f04a9a712cfbd63262e6e0a2
djdeo <deoseoer@gmail.com>
2021-03-03 15:33:58 +0800

    feat: add router4

---
+++ src/components/About.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="about">
+    <p>The about page</p>
+    {{ state.count }}
+  </div>
+</template>
+
+<script setup>
+import { defineProps, reactive } from "vue";
+
+defineProps({
+  msg: String,
+});
+
+const state = reactive({ count: 0 });
+</script>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
